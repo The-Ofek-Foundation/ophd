b5277a13c346ead5fad98925b66706ca9d384d6d
Ofek Gila <ofek@theofekfoundation.org>
2023-06-23 19:13:56 -0700

    Improve about screen

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,77 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class AboutPage extends StatelessWidget {
-  const AboutPage({super.key});
+  const AboutPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-  
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        // add rounded image assets/images/profile.jpg
-        ClipRRect(
-          borderRadius: BorderRadius.circular(125),
-          child: Image.asset(
-            'assets/images/profile.jpg',
-            width: 250,
-            height: 250,
-          ),
-        ),
-        Text(
-          'Ofek Gila',
-          style: Theme.of(context).textTheme.displayLarge,
-        ),
-        Text(
-          'Ph.D. Student, Computer Science @ UCI',
-          style: Theme.of(context).textTheme.titleMedium,
-        ),
-        // add social links using font awesome
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              icon: const Icon(FontAwesomeIcons.github),
-              onPressed: () => launchUrlString('https://github.com/ofekih'),
-              tooltip: 'GitHub',
-            ),
-            IconButton(
-              icon: const Icon(FontAwesomeIcons.linkedin),
-              onPressed: () => launchUrlString('https://www.linkedin.com/in/ofek-gila/'),
-              tooltip: 'LinkedIn',
-            ),
-            IconButton(
-              icon: const Icon(FontAwesomeIcons.envelope),
-              onPressed: () => launchUrlString('mailto:me@ofek.phd'),
-              tooltip: 'Email',
-            ),
-            IconButton(
-              icon: const Icon(FontAwesomeIcons.whatsapp),
-              onPressed: () => launchUrlString('https://wa.me/14087056117'),
-              tooltip: 'WhatsApp',
-            ),
-            IconButton(
-              icon: const Icon(Icons.article),
-              onPressed: () => launchUrlString('https://blog.theofekfoundation.org/'),
-              tooltip: 'Blog',
-            ),
-            IconButton(
-              icon: const Icon(Icons.web),
-              onPressed: () => launchUrlString('https://theofekfoundation.org'),
-              tooltip: 'Website',
-            ),
-            // family tree
-            IconButton(
-              icon: const Icon(Icons.family_restroom),
-              onPressed: () => launchUrlString('https://gila.family/'),
-              tooltip: 'Family',
-            ),
-          ],
-        ),        
-      ],
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth > 500) {
+            return Center(
+              child: SingleChildScrollView(
+                child: _buildCard(),
+              ),
+            );
+          } else {
+            return Center(
+              child: SingleChildScrollView(
+                child: _buildContent()
+              )
+            );
+          }
+        },
+      ),
     );
+  }
+
+  Card _buildCard() {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: _buildContent()
+    );
+  }
+
+  Padding _buildContent() {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(125),
+            child: Image.asset(
+              'assets/images/profile.jpg',
+              width: 250,
+              height: 250,
+            ),
+          ),
+          const Text(
+            'Ofek Gila',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          const Text(
+            'Ph.D. Student, Computer Science @ UCI',
+            style: TextStyle(fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 10),
+          Wrap(
+            alignment: WrapAlignment.center,
+            spacing: 10,
+            children: [
+              _buildIconButton(FontAwesomeIcons.github, 'https://github.com/ofekih', 'GitHub'),
+              _buildIconButton(FontAwesomeIcons.linkedin, 'https://www.linkedin.com/in/ofek-gila/', 'LinkedIn'),
+              _buildIconButton(FontAwesomeIcons.envelope, 'mailto:me@ofek.phd', 'Email'),
+              _buildIconButton(FontAwesomeIcons.whatsapp, 'https://wa.me/14087056117', 'WhatsApp'),
+              _buildIconButton(Icons.article, 'https://blog.theofekfoundation.org/', 'Blog'),
+              _buildIconButton(Icons.web, 'https://theofekfoundation.org', 'Website'),
+              _buildIconButton(Icons.family_restroom, 'https://gila.family/', 'Family'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  IconButton _buildIconButton(IconData icon, String url, String tooltip) {
+    return IconButton(
+      icon: Icon(icon),
+      onPressed: () => _launchURL(url),
+      tooltip: tooltip,
+    );
+  }
+
+  Future<void> _launchURL(String url) async {
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }

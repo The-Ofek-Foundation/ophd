40ef1306073b0a283db68df9463153a38764610b
Ofek Gila <ofek.gila@gmail.com>
2025-08-18 17:28:13 -0700

    Remove deprecated code

--- lib/utils/screen_utils_web.dart
+++ lib/utils/screen_utils_web.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/services.dart';
 import 'package:web/web.dart' as web;
-import 'package:js/js_util.dart' as js_util;
+import 'dart:js_interop';
 
 Future<void> launchAssetInNewTab(String assetPath, String fileType) async {
   final byteData = await rootBundle.load(assetPath);
   final bytes = byteData.buffer.asUint8List();
-  // Convert the Dart list to a JSArray<BlobPart> for the web.Blob constructor.
-  final parts = js_util.jsify([bytes]);
+  final parts = <web.BlobPart>[bytes.toJS].toJS;
   final blob = web.Blob(parts, web.BlobPropertyBag(type: fileType));
   final objectUrl = web.URL.createObjectURL(blob);
   web.window.open(objectUrl, '_blank');

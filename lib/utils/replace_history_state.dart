43b94a5239b641550cbeca441a59163f0a9bf858
Ofek Gila <ofek.gila@gmail.com>
2025-08-22 11:49:04 -0700

    Fix conditional imports

--- lib/utils/replace_history_state.dart
+++ lib/utils/replace_history_state.dart
@@ -1,3 +1,3 @@
 // Conditional export for web-specific history state replacement.
 export 'replace_history_state_stub.dart'
-  if (dart.library.html) 'replace_history_state_web.dart';
+if (dart.library.js_interop) 'replace_history_state_web.dart';
--- lib/utils/screen_utils.dart
+++ lib/utils/screen_utils.dart
@@ -1,6 +1,6 @@
 // Shared utilities with conditional platform-specific implementation for web-only features.
 import 'screen_utils_stub.dart'
-    if (dart.library.html) 'screen_utils_web.dart' as impl;
+if (dart.library.js_interop) 'screen_utils_web.dart' as impl;
 import 'package:url_launcher/url_launcher_string.dart';
 
 Future<void> launchURL(String url) async {
